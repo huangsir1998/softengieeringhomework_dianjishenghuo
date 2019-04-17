85c26c6eb8cf5dc02b61a8a1f68b0fba9fd1075b
liminglong <liminglong_miku@163.com>
2019-04-17 15:09:45 +0800

    delete WEB-INF

--- web/WEB-INF/classes/util/UTF8Helper.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.io.UnsupportedEncodingException;
-
-public class UTF8Helper {
-    public static String parseUTF8(String s) throws UnsupportedEncodingException {
-        return new String(s.getBytes("iso-8859-1"), "utf-8");
-    }
-}
